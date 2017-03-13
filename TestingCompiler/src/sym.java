0fe2e9d7bfa4e31ac5ad39aa850361c11ef4f85f
Gabriel Paz <jgpaz5@gmail.com>
2017-03-13 11:05:16 -0600

    Lex and Sym

---
+++ TestingCompiler/src/sym.java
@@ -0,0 +1,14 @@
+public class sym
+{
+	public static final int EOF = 0;
+	public static final int error = 1;
+	public static final int num = 2;
+	public static final int PLUS = 3;
+	public static final String[] terminalNames = new String[]
+	{
+		"$",
+		"error",
+		"num",
+		"PLUS"
+	};
+}
